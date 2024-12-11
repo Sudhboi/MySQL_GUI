bbaa73db73b969fb38ab4e7b6c9520556a4cd4c6
Sudhboi <sudhboiii@gmail.com>
2024-12-12 01:11:55 +0530

    added basic connectivity

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import _tkinter
+import mysql.connector as sqltor
+
+class Conn:
+    def close(self):
+        print("Connection does not exist.")
+        return None
+
+mycon = Conn()
+
+pwd = input("Enter Password: ")
+
+try:
+    mycon = sqltor.connect(user='root', host='localhost', password=pwd)
+except Exception as e:
+    print("Exception:", e)
+
+mycon.close()
