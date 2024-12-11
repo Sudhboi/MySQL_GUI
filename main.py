e7326e692199bdd148f09e6f24dac67ae47b0dd5
Sudhboi <sudhboiii@gmail.com>
2024-12-12 01:31:13 +0530

    Added Basic Widgets

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import _tkinter
+from tkinter import *
+from tkinter import ttk
 import mysql.connector as sqltor
 
 class Conn:
@@ -7,12 +8,41 @@ class Conn:
         return None
 
 mycon = Conn()
-
-pwd = input("Enter Password: ")
+pwd = 'sqltime'
 
 try:
     mycon = sqltor.connect(user='root', host='localhost', password=pwd)
+    mycur = mycon.cursor()
 except Exception as e:
     print("Exception:", e)
 
+def execute(*args):
+    try:
+        cmd = sqlcommand.get()
+        mycur.execute(cmd)
+    except ValueError:
+        pass
+    except Exception as e:
+        print(e)
+
+
+root = Tk()
+root.title("MySQL GUI")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+sqlcommand = StringVar()
+cmd_entry = ttk.Entry(root, width=50, textvariable=sqlcommand)
+cmd_entry.grid(column=0, row=0, rowspan=3, sticky=(W, E), padx=20, pady=30)
+
+enter_button = ttk.Button(root, width=10, text="Execute", command=execute)
+enter_button.grid(row=0, column=3, padx=20, pady=30)
+
+cmd_entry.focus()
+root.bind("<Return>", execute)
+
+root.mainloop()
 mycon.close()
